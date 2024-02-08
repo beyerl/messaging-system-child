ddc97b96ad212d10fa1d121f5d3d5ec4ce9aeade
lorenz.beyer@orca-software.com <lorenz.beyer@orca-software.com>
2024-02-08 17:49:37 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,28 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    if ((window as any)?.chrome?.webview) {
+      (window as any).chrome.webview.addEventListener('message', (event: MessageEvent<any>) => {
+        this.handleMessage(event)
+      });
+    }
+
+  }
   selectedItem: number | null = null;
   hostLogs: string[] = [];
 
   @HostListener('window:message', ['$event'])
   onMessage(event: MessageEvent): void {
+    this.handleMessage(event)
+  }
+
+  handleMessage(event: MessageEvent) {
     if (event.data === 'sendselecteditem' && event instanceof MessageEvent) {
       this.sendSelectedItemToHost();
     }
